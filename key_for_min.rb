b7025b192e2f4e8170e9e2bff51724d4de35bc41
Sharon Watkins <swatkins876@gmail.com>
2019-09-14 19:04:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,6 +3,8 @@
 
 ikea = {:chair => 25, :table => 85, :mattress => 450}
 
-def key_for_min_value(ikea)
- puts ikea.min?
+def key_for_min_value(ikea) 
+ ikea.collect do |key, vaule|
+   puts key.min? 
+end
 end
