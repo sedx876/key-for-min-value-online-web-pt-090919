bec9f479696542cf513093d7a7f2c28703ad6475
Sharon Watkins <swatkins876@gmail.com>
2019-09-14 19:12:57 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,9 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
+pet_store = {:dog => 10, :cat => 20, :turtle => 30}
 
+def key_for_min_value(pet_store) 
+ pet_store.each { |pet, price| puts pet.min? }
+end
 end
